868b4627b7b10d7e60a8a08549f1137e29b59c05
carlos philipe <carlosphbahia@gmail.com>
2017-02-16 13:49:47 -0800

    Views e controller do situação

---
+++ frontend/views/situacao/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Situacao */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="situacao-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'tipo')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
