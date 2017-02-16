640598b2b377b9c40c8877ad4c8d13c7e394ef70
carlos philipe <carlosphbahia@gmail.com>
2017-02-15 21:25:40 -0800

    adicionado tema LTE ao projeto

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'view' => [
+          'theme' => [
+              'pathMap' => [
+                  '@app/views' => '@common/resources/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+              ],
+          ],
+        ],
     ],
 ];
